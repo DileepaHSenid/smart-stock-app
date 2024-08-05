3e9f5c1906de97f18e2247378108bb529df40364
AlthafNaz <althaf.n@hsenidlanka.com>
2024-08-05 09:23:04 +0530

    Load Suppliers

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -1,8 +1,9 @@
 class ApiEndpoints {
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'http://10.0.2.2:8080';
   static _AuthEndpoints authEndpoints = _AuthEndpoints();
 }
 
 class _AuthEndpoints {
   final String loginEmail = '/signin';
+  final String getSuppliers = '/suppliers';
 }
