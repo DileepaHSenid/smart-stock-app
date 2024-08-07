06057235ee7c79a8d2264731095e5fd22416a88a
DileepaHSenid <dileepa.m@hsenidlanka.com>
2024-08-07 10:51:28 +0530

    Added product view UI

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -45,4 +45,6 @@ class ProductController {
       rethrow;
     }
   }
+
+  deleteProduct(String productId) {}
 }
--- lib/ui/products/products.dart
+++ lib/ui/products/products.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:project/components/appbar.dart';
 import '../../controllers/product_controller.dart';
 
-@override
-Widget build(BuildContext context) {
-  return const MaterialApp(
-    home: Products(),
-    debugShowCheckedModeBanner: false,
-  );
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Products(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
 
 class Products extends StatelessWidget {
@@ -31,16 +39,17 @@ class ProductsPage extends StatefulWidget {
 }
 
 class _ProductsPageState extends State<ProductsPage> {
-  String selectedFilter = "All Products";
-  List<String> filters = ["All Products", "Category 1", "Category 2"];
   List<Map<String, dynamic>> products = [];
+  List<Map<String, dynamic>> filteredProducts = [];
   bool isLoading = true;
   final ProductController productController = ProductController();
+  final TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     fetchProducts();
+    searchController.addListener(_filterProducts);
   }
 
   Future<void> fetchProducts() async {
@@ -48,6 +57,7 @@ class _ProductsPageState extends State<ProductsPage> {
       final fetchedProducts = await productController.fetchProducts();
       setState(() {
         products = fetchedProducts;
+        filteredProducts = fetchedProducts;
         isLoading = false;
       });
     } catch (error) {
@@ -56,6 +66,144 @@ class _ProductsPageState extends State<ProductsPage> {
         isLoading = false;
       });
     }
+  }
+
+  Future<void> deleteProduct(String productId) async {
+    try {
+      final success = await productController.deleteProduct(productId);
+      if (success) {
+        setState(() {
+          products.removeWhere((product) => product['id'] == productId);
+          filteredProducts.removeWhere((product) => product['id'] == productId);
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Product deleted successfully!')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to delete product.')),
+        );
+      }
+    } catch (error) {
+      print('Error deleting product: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $error')),
+      );
+    }
+  }
+
+  void _filterProducts() {
+    final query = searchController.text.toLowerCase();
+    setState(() {
+      filteredProducts = products.where((product) {
+        final productName = product['name'].toLowerCase();
+        return productName.contains(query);
+      }).toList();
+    });
+  }
+
+  void _showProductDetails(Map<String, dynamic> product) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          title: Text('${product['name']}'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildDetailRow('Price:', '\$${product['price']}'),
+              _buildDetailRow('Category:', product['categoryId']),
+              _buildDetailRow('description:', product['description']),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Implement the edit functionality here
+              },
+              child: const Text('Edit'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _showDeleteConfirmationDialog(product['id']);
+              },
+              child: const Text(
+                'Delete',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(String productId) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          title: const Text('Delete Product'),
+          content: const Text('Are you sure you want to permanently delete this product?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                deleteProduct(productId);
+              },
+              child: const Text('Yes'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('No'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildDetailRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Text(
+            '$label ',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[700],
+            ),
+          ),
+          Flexible(
+            child: Text(
+              value,
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -67,33 +215,22 @@ class _ProductsPageState extends State<ProductsPage> {
         children: [
           Row(
             children: [
-              DropdownButton<String>(
-                value: selectedFilter,
-                icon: const Icon(Icons.arrow_downward),
-                items: filters.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedFilter = newValue!;
-                  });
-                },
-              ),
               const Spacer(),
               ElevatedButton.icon(
                 icon: const Icon(Icons.add),
                 label: const Text("Add Product"),
                 onPressed: () {
-                  // Add Product action
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const AddProductPage()),
+                  // );
                 },
               ),
             ],
           ),
           const SizedBox(height: 16),
           TextField(
+            controller: searchController,
             decoration: InputDecoration(
               labelText: 'Search',
               prefixIcon: const Icon(Icons.search),
@@ -107,7 +244,7 @@ class _ProductsPageState extends State<ProductsPage> {
               ? const Center(child: CircularProgressIndicator())
               : Expanded(
             child: ListView.builder(
-              itemCount: products.length,
+              itemCount: filteredProducts.length,
               itemBuilder: (context, index) {
                 return Card(
                   color: const Color(0x80E7D1FF),
@@ -118,13 +255,11 @@ class _ProductsPageState extends State<ProductsPage> {
                     leading: const CircleAvatar(
                       child: Icon(Icons.shopping_bag),
                     ),
-                    title: Text(products[index]['name']),
+                    title: Text(filteredProducts[index]['name']),
                     subtitle: Text(
-                        'Price: \$${products[index]['price']}\nCategory: ${products[index]['category']}'),
+                        'Price: \$${filteredProducts[index]['price']}\nCategory: ${filteredProducts[index]['categoryId']}'),
                     trailing: const Icon(Icons.arrow_forward),
-                    onTap: () {
-                      // Navigate to product details
-                    },
+                    onTap: () => _showProductDetails(filteredProducts[index]),
                   ),
                 );
               },
